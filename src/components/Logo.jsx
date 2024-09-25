fe62803f52da275b70d17de8cd2848935c2720be
Nino Akut <akutnino@gmail.com>
2024-09-26 05:03:01 +0800

    minor changes to the package.json and Logo.jsx

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from '../styles/Logo.module.scss';
+import logo from '../../public/logo.png';
 
 export default function Logo() {
 	return (
 		<Link to={'/'}>
 			<img
-				src='/logo.png'
+				src={logo}
 				alt='WorldWise logo'
 				className={styles.logo}
 			/>
